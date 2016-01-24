fdf505fd622bed87e99c7e63bb0c003792565ddc
coralam17@gmail.com <coralam17@gmail.com>
2016-01-23 23:41:41 -0500

    Draw route between two points

---
+++ public/direction.js
@@ -0,0 +1,17 @@
+function calcRoute() {
+  var start = new google.maps.LatLng(startEnd.lat1, startEnd.lng1);
+  var end = new google.maps.LatLng(startEnd.lat2, startEnd.lng2);
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.WALKING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      directionsDisplay.setMap(map);
+    } else {
+      alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+    }
+  });
+}
